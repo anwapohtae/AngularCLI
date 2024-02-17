2986d9d8e8c89afdeec6c97fdd6333b54d5ee8e5
Anwa Pohtae <anwa.p2544@gmail.com>
2024-02-17 14:34:16 +0700

    editappModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './resource/login/login.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       LoginComponent
    ],
